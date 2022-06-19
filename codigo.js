2e701e88b2b290b19b025cd78598724f07afc654
Omar Carrasco <omarcarrascoguerrero@gmail.com>
2022-06-19 08:59:41 -0500

    Agregando peste negra

---
+++ codigo.js
@@ -0,0 +1,11 @@
+const pesteNegra = document.querySelector(".item__link");
+const title = document.querySelector(".title");
+const nav = document.querySelector(".nav");
+const img = document.querySelector(".item__img");
+console.log(pesteNegra,title)
+
+img.addEventListener("click",(e)=>{
+    
+
+})
+
